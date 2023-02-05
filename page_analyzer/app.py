e1a8d20d34a0cbd0f911d854ba46ba6e8d7183f9
Paul Nikolaevskiy <paulnikolaevsky@gmail.com>
2023-02-05 16:16:41 +0300

    Added a full-fledged site checker.

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,8 +1,10 @@
 import os
 import psycopg2
+import requests
 import validators.url
 from typing import Union
 from datetime import date
+from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 from psycopg2.extras import NamedTupleCursor
 from dotenv import load_dotenv
@@ -33,7 +35,7 @@ class Database:
 
     def get_urls_data(self) -> NamedTupleCursor:
         self.curs.execute(
-            'SELECT '
+            'SELECT DISTINCT ON (urls.id)'
             'urls.id, urls.name, url_checks.status_code, '
             'MAX(url_checks.created_at) as last_check '
             'FROM urls LEFT JOIN url_checks '
@@ -69,10 +71,38 @@ class Database:
         checks_data = self.curs.fetchall()
         return checks_data
 
-    def create_check_entry(self, url_id: int):
+    def create_check_entry(
+            self, url_id: int, h1=None,
+            title=None, description=None
+            ):
         self.curs.execute(
-            'INSERT INTO url_checks (url_id, created_at) VALUES (%s, %s)',
-            (url_id, date.today())
+            'SELECT name FROM urls WHERE id=%s',
+            (url_id,)
+        )
+        url_name = self.curs.fetchone().name
+
+        resp = requests.get(url_name)
+        status_code = resp.status_code
+
+        html = resp.text
+        soup = BeautifulSoup(html)
+
+        if soup.select('h1'):
+            h1 = soup.select('h1')[0].text.strip()
+
+        if soup.select('title'):
+            title = soup.select('title')[0].text.strip()
+
+        if soup.find('meta', {"name": "description"}):
+            description = soup.find(
+                'meta', {"name": "description"}
+            ).attrs['content']
+
+        self.curs.execute(
+            'INSERT INTO url_checks '
+            '(url_id, status_code, h1, title, description, created_at) '
+            'VALUES (%s, %s, %s, %s, %s, %s)',
+            (url_id, status_code, h1, title, description, date.today())
         )
         self.save()
 
