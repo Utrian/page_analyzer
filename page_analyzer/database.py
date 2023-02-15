cf6fe0c98694a598f31e8fbdb358798e25bb75ba
Paul Nikolaevskiy <paulnikolaevsky@gmail.com>
2023-02-15 17:54:42 +0300

    Added context managers for database queries.

--- page_analyzer/database.py
+++ page_analyzer/database.py
@@ -11,48 +11,53 @@ class Database:
     def __init__(self):
         self.DATABASE_URL = os.getenv('DATABASE_URL')
         self.conn = psycopg2.connect(self.DATABASE_URL)
-        self.curs = self.conn.cursor(cursor_factory=NamedTupleCursor)
 
     def save(self):
         self.conn.commit()
 
     def close(self):
-        self.curs.close()
         self.conn.close()
 
 
 class Urls(Database):
     def get_urls_data(self) -> NamedTupleCursor:
-        self.curs.execute(
-            'SELECT DISTINCT ON (urls.id)'
-            'urls.id, urls.name, url_checks.status_code, '
-            'MAX(url_checks.created_at) as last_check '
-            'FROM urls LEFT JOIN url_checks '
-            'ON urls.id = url_checks.url_id '
-            'GROUP BY urls.id, url_checks.status_code '
-            'ORDER BY urls.id DESC;'
-        )
+        with self.conn as conn:
+            with conn.cursor(cursor_factory=NamedTupleCursor) as curs:
+                curs.execute(
+                    'SELECT DISTINCT ON (urls.id)'
+                    'urls.id, urls.name, url_checks.status_code, '
+                    'MAX(url_checks.created_at) as last_check '
+                    'FROM urls LEFT JOIN url_checks '
+                    'ON urls.id = url_checks.url_id '
+                    'GROUP BY urls.id, url_checks.status_code '
+                    'ORDER BY urls.id DESC;'
+                )
+                urls_data = curs.fetchall()
 
-        urls_data = self.curs.fetchall()
         return urls_data
 
     def find_url(self, url: Union[str, int]) -> NamedTupleCursor:
         if isinstance(url, str):
-            self.curs.execute('SELECT * FROM urls WHERE name=%s', (url,))
+            where = 'name=' + str(url)
         else:
-            id = url
-            self.curs.execute('SELECT * FROM urls WHERE id=%s', (id,))
+            where = 'id=' + str(url)
 
-        url_data = self.curs.fetchone()
+        with self.conn as conn:
+            with conn.cursor(cursor_factory=NamedTupleCursor) as curs:
+                curs.execute(f'SELECT * FROM urls WHERE {where}')
+                url_data = curs.fetchone()
+
         return url_data
 
     def create_url_entry(self, url_name: str):
-        self.curs.execute(
-            'INSERT INTO urls (name, created_at) VALUES (%s, %s)',
-            (url_name, date.today())
-        )
+        with self.conn as conn:
+            with conn.cursor(cursor_factory=NamedTupleCursor) as curs:
+                curs.execute(
+                    'INSERT INTO urls (name, created_at) VALUES (%s, %s)',
+                    (url_name, date.today())
+                )
+                self.save()
 
-        self.save()
         return self.find_url(url_name)
 
 
@@ -83,27 +88,37 @@ def get_resp_data(url_name):
 
 class Checks(Database):
     def find_checks(self, url_id: int) -> NamedTupleCursor:
-        self.curs.execute(
-            'SELECT * FROM url_checks WHERE url_id=%s ORDER BY id DESC',
-            (url_id,)
-        )
+        with self.conn:
+            with self.conn.cursor(cursor_factory=NamedTupleCursor) as curs:
+                curs.execute(
+                    'SELECT * FROM url_checks '
+                    'WHERE url_id=%s ORDER BY id DESC',
+                    (url_id,)
+                )
+                checks_data = curs.fetchall()
 
-        checks_data = self.curs.fetchall()
         return checks_data
 
     def create_check_entry(self, url_id: int):
-        self.curs.execute(
-            'SELECT name FROM urls WHERE id=%s',
-            (url_id,)
-        )
-        url_name = self.curs.fetchone().name
+        with self.conn:
+            with self.conn.cursor(cursor_factory=NamedTupleCursor) as curs:
+                curs.execute(
+                    'SELECT name FROM urls WHERE id=%s',
+                    (url_id,)
+                )
+                url_name = curs.fetchone().name
 
         status_code, h1, title, description = get_resp_data(url_name)
 
-        self.curs.execute(
-            'INSERT INTO url_checks '
-            '(url_id, status_code, h1, title, description, created_at) '
-            'VALUES (%s, %s, %s, %s, %s, %s)',
-            (url_id, status_code, h1, title, description, date.today())
-        )
-        self.save()
+        with self.conn:
+            with self.conn.cursor(cursor_factory=NamedTupleCursor) as curs:
+                curs.execute(
+                    'INSERT INTO url_checks '
+                    '('
+                    'url_id, status_code, h1, '
+                    'title, description, created_at'
+                    ') '
+                    'VALUES (%s, %s, %s, %s, %s, %s)',
+                    (url_id, status_code, h1, title, description, date.today())
+                )
+                self.save()
