a68d51df61a0ed5dd0bb707ab50bce5c7519a079
Paul Nikolaevskiy <paulnikolaevsky@gmail.com>
2023-02-17 14:00:50 +0300

    Changed get_check_data.

--- page_analyzer/parser.py
+++ page_analyzer/parser.py
@@ -6,23 +6,21 @@ def get_check_data(url_id, url_name):
     resp = requests.get(url_name)
     status_code = resp.status_code
 
-    if status_code != 200:
-        raise requests.exceptions.RequestException
-
     html = resp.text
     soup = BeautifulSoup(html, features="html.parser")
 
     h1, title, description = None, None, None
 
     if soup.find('h1'):
-        h1 = soup.find('h1').text.strip()
+        h1 = soup.find('h1').get_text(strip=True)
 
     if soup.find('title'):
-        title = soup.find('title').text.strip()
+        title = soup.find('title').get_text(strip=True)
 
     if soup.find('meta', {"name": "description"}):
-        description = soup.find('meta', {"name": "description"})
-        description = description.attrs['content']
+        description = soup.find(
+            'meta', {"name": "description"}
+            ).get('content')
 
     return {
         'id': url_id,
